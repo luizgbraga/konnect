83a240c2595bb0f9da80fbed138e27a71408c9c2
luizgbraga <luizb.07@outlook.com>
2024-04-16 22:39:52 -0300

    wtf

--- src/main/java/com/social_network/server/views/Home.java
+++ src/main/java/com/social_network/server/views/Home.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(name = "home", value = "/")
+@WebServlet(name = "home", value = "/home")
 public class Home extends HttpServlet {
     public void init() {}
 
